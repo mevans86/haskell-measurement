0b8f5eb28a849703ff33bdd5c64a5aac8b5da867
Michael Evans <mevans@metallacycle.com>
2013-08-02 17:38:35 -0400

    Show refactored, unitToThePowerOf, partial prefix handling
    showPretty and showFundamental implemented, unitToThePowerOf
    implemented to raise units to powers (but raising Quants to powers
    remains unsupported…), prefix and shiftPrefix implemented to handle
    prefixes

--- Measurement.hs
+++ Measurement.hs
@@ -1,86 +1,67 @@
 module Measurement
 (Quant,
+    coulomb,
+    farad,
+    hertz,
+    joule,
+    newton,
+    ohm,
+    pascal,
+    volt,
+    watt,
     plus,
     minus,
     times,
     dividedBy,
     readFromString,
+    shiftPrefix,
+    showFundamental,
+    showPretty,
     unitTimes,
+    unitToThePowerOf,
     unitOver) where
 
-data Quant a = Quant { value :: a, unit :: [(Float, Integer)] } | Nothing deriving (Eq, Read)
-
-instance (Show a, Eq a) => Show (Quant a) where
-    show q
-        | q == Measurement.Nothing = "Nothing"
-        | otherwise = (show . value $ q) ++ " " ++ kg ++ m ++ mol ++ amp ++ cd ++ k ++ s
-            where kg
-                    | snd (head (unit q)) == 0 = ""
-                    | snd (head (unit q)) == 1 = "kg"
-                    | otherwise = "kg^" ++ show (snd (head (unit q)))
-                  m
-                    | snd (unit q !! 1) == 0 = ""
-                    | snd (unit q !! 1) == 1 = " m"
-                    | otherwise = " m^" ++ show (snd (unit q !! 1))
-                  s
-                    | snd (unit q !! 2) == 0 = ""
-                    | snd (unit q !! 2) == 1 = " s"
-                    | otherwise = " s^" ++ show (snd (unit q !! 2))
-                  k
-                    | snd (unit q !! 3) == 0 = ""
-                    | snd (unit q !! 3) == 1 = " K"
-                    | otherwise = " K^" ++ show (snd (unit q !! 3))
-                  mol
-                    | snd (unit q !! 4) == 0 = ""
-                    | snd (unit q !! 4) == 1 = " mol"
-                    | otherwise = " mol^" ++ show (snd (unit q !! 4))
-                  amp
-                    | snd (unit q !! 5) == 0 = ""
-                    | snd (unit q !! 5) == 1 = " A"
-                    | otherwise = " A^" ++ show (snd (unit q !! 5))
-                  cd
-                    | snd (unit q !! 6) == 0 = ""
-                    | snd (unit q !! 6) == 1 = " cd"
-                    | otherwise = " cd^" ++ show (snd (unit q !! 6))
+type QuantUnit = [(Integer, Integer)]
+data Quant a = Quant { value :: a, unit :: QuantUnit } | Nothing deriving (Eq, Read, Show)
 
 -- Dimensionless units
-unity :: [(Float, Integer)]
+unity :: QuantUnit
 unity = replicate 7 (1, 0)
 
 -- SI units
-kilogram :: [(Float, Integer)]
-kilogram = [(1,1), (1,0), (1,0), (1,0), (1,0), (1,0), (1,0)]
-meter :: [(Float, Integer)]
-meter = [(1,0), (1,1), (1,0), (1,0), (1,0), (1,0), (1,0)]
-second :: [(Float, Integer)]
-second = [(1,0), (1,0), (1,1), (1,0), (1,0), (1,0), (1,0)]
-kelvin :: [(Float, Integer)]
-kelvin = [(1,0), (1,0), (1,0), (1,1), (1,0), (1,0), (1,0)]
-mole :: [(Float, Integer)]
-mole = [(1,0), (1,0), (1,0), (1,0), (1,1), (1,0), (1,0)]
-ampere :: [(Float, Integer)]
-ampere = [(1,0), (1,0), (1,0), (1,0), (1,0), (1,1), (1,0)]
-candela :: [(Float, Integer)]
-candela = [(1,0), (1,0), (1,0), (1,0), (1,0), (1,0), (1,1)]
+kilogram :: QuantUnit
+kilogram = [(3,1), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0)]
+meter :: QuantUnit
+meter = [(0, 0), (0,1), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0)]
+second :: QuantUnit
+second = [(0, 0), (0, 0), (0,1), (0, 0), (0, 0), (0, 0), (0, 0)]
+kelvin :: QuantUnit
+kelvin = [(0, 0), (0, 0), (0, 0), (0,1), (0, 0), (0, 0), (0, 0)]
+mole :: QuantUnit
+mole = [(0, 0), (0, 0), (0, 0), (0, 0), (0,1), (0, 0), (0, 0)]
+ampere :: QuantUnit
+ampere = [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0,1), (0, 0)]
+candela :: QuantUnit
+candela = [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0,1)]
 
 -- Derived units
-coulomb :: [(Float, Integer)]
-coulomb = zipWith (\(pre1, pow1) (pre2, pow2) -> (pre1 * pre2, pow1 + pow2)) ampere second
-farad :: [(Float, Integer)]
+coulomb :: QuantUnit
+coulomb = zipWith (\(pre1, pow1) (pre2, pow2) -> (pre1 + pre2, pow1 + pow2)) ampere second
+farad :: QuantUnit
 farad = coulomb `unitOver` volt
-hertz :: [(Float, Integer)]
+hertz :: QuantUnit
 hertz = unity `unitOver` second
-joule :: [(Float, Integer)]
-joule = zipWith (\(pre1, pow1) (pre2, pow2) -> (pre1 * pre2, pow1 + pow2)) newton meter
-newton :: [(Float, Integer)]
-newton = zipWith (\(pre1, pow1) (pre2, pow2) -> (pre1 * pre2, -2 * pow1 + pow2)) second . zipWith (\(pre1, pow1) (pre2, pow2) -> (pre1 * pre2, pow1 + pow2)) kilogram $ meter
-ohm :: [(Float, Integer)]
+joule :: QuantUnit
+joule = zipWith (\(pre1, pow1) (pre2, pow2) -> (pre1 + pre2, pow1 + pow2)) newton meter
+newton :: QuantUnit
+newton = zipWith (\(pre1, pow1) (pre2, pow2) -> (pre1 + pre2, -2 * pow1 + pow2)) second . zipWith (\(pre1, pow1) (pre2, pow2) -> (pre1 + pre2, pow1 + pow2)) kilogram $ meter
+ohm :: QuantUnit
 ohm = volt `unitOver` ampere
-pascal :: [(Float, Integer)]
-pascal = zipWith (\(pre1, pow1) (pre2, pow2) -> (pre1 * pre2, pow1 - 2 * pow2)) newton meter
-volt :: [(Float, Integer)]
+pascal :: QuantUnit
+pascal = zipWith (\(pre1, pow1) (pre2, pow2) -> (pre1 + pre2, pow1 - 2 * pow2)) newton meter
+volt :: QuantUnit
 volt = watt `unitOver` ampere
-watt :: [(Float, Integer)]
+watt :: QuantUnit
 watt = joule `unitOver` second
 
 plus :: (Num a) => Quant a -> Quant a -> Quant a
@@ -94,20 +75,96 @@ minus q1 q2
     | otherwise = Measurement.Nothing
 
 times :: (Num a) => Quant a -> Quant a -> Quant a
-times q1 q2 = Quant { value = value q1 * value q2, unit = zipWith (\(pre1, pow1) (pre2, pow2) -> (pre1 * pre2, pow1 + pow2)) (unit q1) (unit q2) }
+times q1 q2 = Quant { value = value q1 * value q2, unit = zipWith (\(pre1, pow1) (pre2, pow2) -> (pre1 + pre2, pow1 + pow2)) (unit q1) (unit q2) }
 
 dividedBy :: (Eq a, Fractional a, Num a) => Quant a -> Quant a -> Quant a
 dividedBy q1 q2
     | value q2 == 0 = Measurement.Nothing
-    | otherwise = Quant { value = value q1 / value q2, unit = zipWith (\(pre1, pow1) (pre2, pow2) -> (pre1 / pre2, pow1 - pow2)) (unit q1) (unit q2) }
+    | otherwise = Quant { value = value q1 / value q2, unit = zipWith (\(pre1, pow1) (pre2, pow2) -> (pre1 - pre2, pow1 - pow2)) (unit q1) (unit q2) }
 
 -- Utility Functions
+shiftPrefix :: (Num a, Fractional a) => Integer -> Quant a -> Quant a
+shiftPrefix shift q
+    | length (filter (\(_, pow) -> (pow > 0)) (unit q)) /= 1 = Measurement.Nothing
+    | ((finalPrefix `mod` 3) /= 0) && (abs finalPrefix > 3) = Measurement.Nothing
+    | otherwise = Quant { value = (value q / 10^^shift)^^(sum . map snd $ unit q), unit = map (\(pre, pow) -> (pre + shift, pow)) (unit q) }
+        where finalPrefix = head [ pre | (pre, pow) <- unit q, pow > 0 ] + shift
+
+prefix :: (Num a, Eq a) => (a, b) -> String
+prefix (pre, _)
+    | pre == 24 = "Y"
+    | pre == 21 = "Z"
+    | pre == 18 = "E"
+    | pre == 15 = "P"
+    | pre == 12 = "T"
+    | pre == 9 = "G"
+    | pre == 6 = "M"
+    | pre == 3 = "k"
+    | pre == 2 = "h"
+    | pre == 1 = "da"
+    | pre == -1 = "d"
+    | pre == -2 = "c"
+    | pre == -3 = "m"
+    | pre == -6 = "mc"
+    | pre == -9 = "n"
+    | pre == -12 = "p"
+    | pre == -15 = "f"
+    | pre == -18 = "a"
+    | pre == -21 = "z"
+    | pre == -24 = "y"
+    | otherwise = ""
+
 readFromString :: (Fractional a, Num a, Read a) => String -> Quant a
 readFromString str
-    | length tok >= 2 = Quant { value = val, unit = foldl (zipWith (\(pre1, pow1) (pre2, pow2) -> (pre1 * pre2, pow1 + pow2))) unity (map unitVectorFromString tok) } 
+    | length tok >= 2 = Quant { value = val, unit = foldl (zipWith (\(pre1, pow1) (pre2, pow2) -> (pre1 + pre2, pow1 + pow2))) unity (map unitVectorFromString tok) } 
     | otherwise = Measurement.Nothing
     where tok = words str
           val = read (head tok)
+
+showFundamental :: (Eq a, Show a) => Quant a -> String
+showFundamental q = (show . value $ q) ++ kg ++ m ++ mol ++ amp ++ cd ++ k ++ s
+            where kg
+                    | snd (head (unit q)) == 0 = ""
+                    | snd (head (unit q)) == 1 = " " ++ prefix (head (unit q)) ++ "g"
+                    | otherwise = " " ++ prefix (head (unit q)) ++ "g^" ++ show (snd (head (unit q)))
+                  m
+                    | snd (unit q !! 1) == 0 = ""
+                    | snd (unit q !! 1) == 1 = " " ++ prefix (unit q !! 1) ++ "m"
+                    | otherwise = " " ++ prefix (unit q !! 1) ++ "m^" ++ show (snd (unit q !! 1))
+                  s
+                    | snd (unit q !! 2) == 0 = ""
+                    | snd (unit q !! 2) == 1 = " " ++ prefix (unit q !! 2) ++ "s"
+                    | otherwise = " " ++ prefix (unit q !! 2) ++ "s^" ++ show (snd (unit q !! 2))
+                  k
+                    | snd (unit q !! 3) == 0 = ""
+                    | snd (unit q !! 3) == 1 = " " ++ prefix (unit q !! 3) ++ "K"
+                    | otherwise = " " ++ prefix (unit q !! 3) ++ "K^" ++ show (snd (unit q !! 3))
+                  mol
+                    | snd (unit q !! 4) == 0 = ""
+                    | snd (unit q !! 4) == 1 = " " ++ prefix (unit q !! 4) ++ "mol"
+                    | otherwise = " " ++ prefix (unit q !! 4) ++ "mol^" ++ show (snd (unit q !! 4))
+                  amp
+                    | snd (unit q !! 5) == 0 = ""
+                    | snd (unit q !! 5) == 1 = " " ++ prefix (unit q !! 5) ++ "A"
+                    | otherwise = " " ++ prefix (unit q !! 5) ++ "A^" ++ show (snd (unit q !! 5))
+                  cd
+                    | snd (unit q !! 6) == 0 = ""
+                    | snd (unit q !! 6) == 1 = " " ++ prefix (unit q !! 6) ++ "cd"
+                    | otherwise = " " ++ prefix (unit q !! 6) ++ "cd^" ++ show (snd (unit q !! 6))
+
+showPretty :: (Eq a, Show a) => Quant a -> String
+showPretty q
+        | q == Measurement.Nothing = "Nothing"
+        | unit q == coulomb = (show . value $ q) ++ " C"
+        | unit q == farad = (show . value $ q) ++ " F"
+        | unit q == hertz = (show . value $ q) ++ " Hz"
+        | unit q == joule = (show . value $ q) ++ " J"
+        | unit q == newton = (show . value $ q) ++ " N"
+        | unit q == ohm = (show . value $ q) ++ " ohm"
+        | unit q == pascal = (show . value $ q) ++ " Pa"
+        | unit q == volt = (show . value $ q) ++ " V"
+        | unit q == watt = (show . value $ q) ++ " W"
+        | otherwise = showFundamental q
 
 splitWhen :: (Char -> Bool) -> String -> [String]
 splitWhen p s =  case dropWhile p s of
@@ -115,13 +172,16 @@ splitWhen p s =  case dropWhile p s of
                       s' -> w : splitWhen p s''
                             where (w, s'') = break p s'
 
-unitOver :: [(Float, Integer)] -> [(Float, Integer)] -> [(Float, Integer)]
-unitOver = zipWith (\(pre1, pow1) (pre2, pow2) -> (pre1 / pre2, pow1 - pow2))
+unitOver :: QuantUnit -> QuantUnit -> QuantUnit
+unitOver = zipWith (\(pre1, pow1) (pre2, pow2) -> (pre1 - pre2, pow1 - pow2))
 
-unitTimes :: [(Float, Integer)] -> [(Float, Integer)] -> [(Float, Integer)]
-unitTimes = zipWith (\(pre1, pow1) (pre2, pow2) -> (pre1 * pre2, pow1 + pow2))
+unitTimes :: QuantUnit -> QuantUnit -> QuantUnit
+unitTimes = zipWith (\(pre1, pow1) (pre2, pow2) -> (pre1 + pre2, pow1 + pow2))
 
-unitVectorFromString :: String -> [(Float, Integer)]
+unitToThePowerOf :: QuantUnit -> Integer -> QuantUnit
+unitToThePowerOf q1 expt = map (\(pre, pow) -> (pre, pow * expt)) q1
+
+unitVectorFromString :: String -> QuantUnit
 unitVectorFromString str = case head unitExp of "kg" -> fmap (\(pre, pow) -> (pre, pow * expt)) kilogram
                                                 "m" -> fmap (\(pre, pow) -> (pre, pow * expt)) meter
                                                 "s" -> fmap (\(pre, pow) -> (pre, pow * expt)) second
--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Simple as that! haskell-measurement takes care of everything behind the scenes. 
 ## Units
 haskell-measurement defines the seven fundamental SI units as constants. Here are their names:
 
-* kilogram *(yes, the unit is the **kilo**gram!)*
+* kilogram *(yes, the unit is the kilogram!)*
 * meter
 * second
 * kelvin
@@ -51,7 +51,7 @@ Basic arithmetic is implemented as the `plus`, `minus`, `times`, and `dividedBy`
 
 haskell-measurement also includes a couple of functions for working with units; these allow one to define complex units directly during the creation of a measurement. `unitTimes` and `unitOver` are fairly self-explanatory: they multiply and divide two units, respectively.
 
-Quant is an instance of the `Show` typeclass, meaning that Quants are automatically shown in "pretty" form in ghci or when using the `show` function.
+Quant is derived instance of Show, but I have also provided a `showPretty` function that outputs a measurement with units in human-readable form. `showPretty` outputs derived units where applicable (derived units raised to powers are not supported, however). `showFundamental` forces a string containing only the seven fundamental units along with the value.
 
 The `readFromString` function takes a single string parameter and returns a Quant. The string has a few formatting requirements: the value of the measurement must come first, followed by a space-separated list of multiplied units. Powers of units are supported, so the following works just fine.
 
@@ -61,15 +61,19 @@ The `readFromString` function takes a single string parameter and returns a Quan
 
 Using scientific notation here is supported; just make sure to write values as, for example, "12.23e-3."
 
-# To Do
-*Prefix handling:* prefix handling to address powers of ten does not work yet. Reading a measurement from a string will only work properly when the fundamental units with no prefixes are used (note that "kg" works fine, since the kilogram is the fundamental unit of mass).
-
-*unitToThePowerOf*: a function for raising a unit to a power without need for an arbitrary computation. The idea is to allow the creation of complex units without unnecessary computations. We'd like to use this...
+The `unitToThePowerOf` function raises a unit to a power without need for an arbitrary computation. The idea is to allow the creation of complex units without unnecessary computations. Now you can use this...
 
 	ghci> Quant { value = 5.0, unit = meter `unitToThePowerOf` 2 }
 	
 Instead of having to do this...
 
 	ghci> Quant { value = 5.0, unit = meter } `times` Quant { value = 1.0, unit = meter }
-	
-*Showing derived units*: at present, only fundamental units are spit out by `show`. Eventually derived units will appear when a given collection of units matches a derived unit.
+
+It works on derived units too!
+
+Prefix handling is partially implemented via the `shiftPrefix` function. This function allows you to move the prefix of a unit a specified amount. At present, it works only on simple (one-dimensional) units; complex units made of more than one fundamental are not supported. Units raised to powers are supported in this context---for example, shifting 1.0 m<sup>2</sup> to cm<sup>2</sup> results in 10000.0 cm<sup>2</sup>.
+
+# To Do
+*Prefix handling:* prefix handling is partially implemented for existing Quants (see above), but reading a measurement from a string will still only work properly when the fundamental units with no prefixes are used (note that "kg" works fine, since the kilogram is the fundamental unit of mass).
+
+*Showing derived units*: the `showPretty` function partially addresses this issue, but powers of derived units remain unsupported.
